3b4d8f1a44e52aac6d77b2ef93506efc9bc01a91
Guilherme Lacerda <83317386+lsguilherme@users.noreply.github.com>
2025-06-24 21:03:15 -0300

    chore: remove import

--- backend/src/main/java/com/example/desafiosenai/entities/ProductCouponApplicationEntity.java
+++ backend/src/main/java/com/example/desafiosenai/entities/ProductCouponApplicationEntity.java
@@ -1,6 +1,5 @@
 package com.example.desafiosenai.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
--- backend/src/main/java/com/example/desafiosenai/services/ProductService.java
+++ backend/src/main/java/com/example/desafiosenai/services/ProductService.java
@@ -10,7 +10,6 @@ import com.example.desafiosenai.entities.ProductEntity;
 import com.example.desafiosenai.repositories.CouponRepository;
 import com.example.desafiosenai.repositories.ProductRepository;
 import com.example.desafiosenai.utils.CodeNormalizer;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonpatch.JsonPatch;
