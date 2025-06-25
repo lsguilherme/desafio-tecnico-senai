246f55ab14bc915a599b87547ab95a746fbd8997
Guilherme Lacerda <83317386+lsguilherme@users.noreply.github.com>
2025-06-25 19:08:27 -0300

    feat: add cors

--- backend/src/main/java/com/example/desafiosenai/config/WebMvcCustomConfigurer.java
+++ backend/src/main/java/com/example/desafiosenai/config/WebMvcCustomConfigurer.java
@@ -3,11 +3,19 @@ package com.example.desafiosenai.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcCustomConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE");
+    }
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer){
